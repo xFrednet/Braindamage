bf7f905daa028072724cfdf92ce67f1879c11083
xFrednet <xFrednet@gmail.com>
2025-05-31 19:53:38 +0200

    Simple run command

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
-mod pass;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(name = "braindamage")]
 struct Cli {
-    file: String,
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    #[arg(short, long, default_value = "a.bf")]
-    output: String,
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Run(RunArgs),
+}
+
+#[derive(Args, Debug)]
+struct RunArgs {
+    // FIXME: Would be cool, if this wouldn't require the `-f` flag
+    /// The file to run
+    #[arg(short, long, required = true)]
+    file: Option<PathBuf>,
+
+    // FIXME: This should take a string, to allow units like
+    // 1kb or 10mb
+    /// The amount of memory which should be provided by the interpreter
+    #[arg(short, long, default_value = "1000")]
+    memory: usize,
+
+    /// The start position of the memory head.
+    #[arg(long, default_value = "100")]
+    start: usize,
+
+    // FIXME: Make default value dependent on build profile.
+    /// Enable Debug Mode
+    #[arg(short, long, default_value = "true")]
+    debug: bool,
 }
 
 fn main() {
     let cli = Cli::parse();
-    println!("Input: {}", cli.file);
-    println!("Output: {}", cli.output);
-
-    let out = pass::run_passes("--REPEAT(x,3)--");
-    println!("{out}");
+    println!("Output: {:#?}", cli);
 }
