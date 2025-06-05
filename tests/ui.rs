24941d2b0616acd94f5a30be2fce5516d22f8d9f
xFrednet <xFrednet@gmail.com>
2025-06-05 21:20:28 +0200

    Move `test/ui/*` -> `test/ui/bf/*`

--- tests/ui.rs
+++ tests/ui.rs
@@ -1,35 +1,41 @@
+use std::path::Path;
+
 use assert_cmd::Command;
 use insta::{assert_snapshot, Settings};
 use regex::Regex;
 
 #[test]
 fn test_bf() {
+    insta::glob!("ui/bf/*.bf", |path| {
+        run_test_file(path);
+    });
+}
+
+fn run_test_file(path: &Path) {
     let mut settings = Settings::new();
     settings.remove_snapshot_suffix();
     settings.set_prepend_module_to_snapshot(false);
+    settings.set_snapshot_path(path.parent().unwrap());
 
-    insta::glob!("ui/*.bf", |path| {
-        let file_stem = path.file_stem().unwrap().to_string_lossy();
+    let file_stem = path.file_stem().unwrap().to_string_lossy();
 
-        let mut com = Command::cargo_bin("braindamage").unwrap();
-        com.arg("run").arg("--file").arg(&path);
+    let mut com = Command::cargo_bin("braindamage").unwrap();
+    com.arg("run").arg("--file").arg(&path);
 
-        let file_content = std::fs::read_to_string(path).unwrap();
-        let re = Regex::new(r"//@arg\s+([^=\n]+)=([^\n]+)").unwrap();
-        for cap in re.captures_iter(&file_content) {
-            com.arg(&cap[1]);
-            com.arg(&cap[2]);
-        }
+    let file_content = std::fs::read_to_string(path).unwrap();
+    let re = Regex::new(r"//@arg\s+([^=\n]+)=([^\n]+)").unwrap();
+    for cap in re.captures_iter(&file_content) {
+        com.arg(&cap[1]);
+        com.arg(&cap[2]);
+    }
 
-        let output = com.output().unwrap();
+    let output = com.output().unwrap();
 
-        let stdout = String::from_utf8_lossy(&output.stdout).trim().to_string();
-        let stderr = String::from_utf8_lossy(&output.stderr).trim().to_string();
+    let stdout = String::from_utf8_lossy(&output.stdout).trim().to_string();
+    let stderr = String::from_utf8_lossy(&output.stderr).trim().to_string();
 
-        settings.set_snapshot_path(path.parent().unwrap());
-        settings.bind(|| {
-            assert_snapshot!(format!("{}.stdout", file_stem), &stdout);
-            assert_snapshot!(format!("{}.stderr", file_stem), &stderr);
-        });
+    settings.bind(|| {
+        assert_snapshot!(format!("{}.stdout", file_stem), &stdout);
+        assert_snapshot!(format!("{}.stderr", file_stem), &stderr);
     });
 }
